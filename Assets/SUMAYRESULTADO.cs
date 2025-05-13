a5db61f6c3f591d3e66a61c5273120e488a178ec
hanono09 <fedehanono@gmail.com>
2025-05-13 09:01:37 -0300

    EJ 4 terminado.

---
+++ Assets/SUMAYRESULTADO.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SUMAYRESULTADO : MonoBehaviour
+{
+    public TMP_InputField InputNum1;
+    public TMP_InputField InputNum2;
+    public TextMeshProUGUI txtResultado;
+    int Num1;
+    int Num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtResultado.text = "";
+    }
+    public void SumarYMostrar()
+    {
+        Num1 = int.Parse(InputNum1.text);
+        Num2 = int.Parse(InputNum2.text);
+        txtResultado.text = (Num1 + Num2).ToString();
+    }
+   
+}
